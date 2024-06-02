4bc02de560260e9f1ff064f648074809f99f176b
myeongeun <c_me95@mentorit.co.kr>
2024-06-02 23:04:08 +0900

    Front-End 생성

--- frontend/src/components/TodoBoard.jsx
+++ frontend/src/components/TodoBoard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TodoBoard = () => {
     return (
         <div>
-            <h2>Todo List</h2>
+            <h2>해야할일 목록</h2>
             {/* <TodoItem/> will be here once we get the todoList */}
             <h2>There is no Item to show</h2>
         </div>
