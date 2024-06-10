a409a4c7723dee651fb3d4e50779d6411b78f598
myeongeun <c_me95@mentorit.co.kr>
2024-06-10 23:32:14 +0900

    Front-End 로그인 기능 심화 페이지별 권한관리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,26 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
+import PrivateRoute from "./route/PrivateRoute";
+import { useState } from "react";
+import api from "./utils/api.js";
 
 function App() {
+    const [user, setUser] = useState(null);
+    const getUser = async () => {
+        try {
+            const token = sessionStorage.getItem("token");
+            // const response = await api.post(`/user/login`, { email, password });
+        } catch (e) {
+
+        }
+    };
 
     return (
         <Routes>
-            <Route path="" element={<TodoPage />} />
+            <Route path="/" element={
+                <PrivateRoute user={ user }><TodoPage /></PrivateRoute>
+            } />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
         </Routes>
